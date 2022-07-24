12a59bf8bb5a12d394c07413c3660d96570e93bb
Pedro Henrique Pereira Sales <sales_pedro@terceiros.sicredi.com.br>
2022-07-24 00:23:26 -0300

    feat: import dialog nodes automatically from source workspace to target workspace

--- src/presentation/config/index.ts
+++ src/presentation/config/index.ts
@@ -4,4 +4,5 @@ dotenv.config({ path: '.env' })
 export const PORT: string = process.env.PORT
 export const WATSON_API_KEY: string = process.env.WATSON_API_KEY
 export const WATSON_SERVICE_URL: string = process.env.WATSON_SERVICE_URL
-export const WATSON_ASSISTANT_WORKSPACE: string = process.env.WATSON_ASSISTANT_WORKSPACE
+export const WATSON_ASSISTANT_SOURCE_WORKSPACE_ID: string = process.env.WATSON_ASSISTANT_SOURCE_WORKSPACE_ID
+export const WATSON_ASSISTANT_TARGET_WORKSPACE_ID: string = process.env.WATSON_ASSISTANT_TARGET_WORKSPACE_ID
--- src/presentation/controllers/create-assistant.ts
+++ src/presentation/controllers/create-assistant.ts
@@ -1,6 +1,6 @@
 import AssistantV1 from 'ibm-watson/assistant/v1'
 import { IamAuthenticator } from 'ibm-watson/auth'
-import { WATSON_SERVICE_URL, WATSON_API_KEY, WATSON_ASSISTANT_WORKSPACE } from '../config/index'
+import { WATSON_SERVICE_URL, WATSON_API_KEY, WATSON_ASSISTANT_TARGET_WORKSPACE_ID, WATSON_ASSISTANT_SOURCE_WORKSPACE_ID } from '../config/index'
 
 export const createAssistantV1 = new AssistantV1({
   version: '2021-06-14',
@@ -10,39 +10,70 @@ export const createAssistantV1 = new AssistantV1({
   serviceUrl: WATSON_SERVICE_URL
 })
 
-export async function getWorkspaceDialogTree (): Promise<any> {
+export async function getTargetWorkspaceDialogTree (): Promise<any> {
   try {
     return await createAssistantV1?.listDialogNodes({
-      workspaceId: WATSON_ASSISTANT_WORKSPACE
+      workspaceId: WATSON_ASSISTANT_TARGET_WORKSPACE_ID
     })
   } catch (listDialogNodesError) {
     return listDialogNodesError
   }
 }
 
-export async function updateWorkspaceDialogTree (workspaceDialogTree: any, newDialogTree: any, entryPointNode: any): Promise<any> {
+export async function updateWorkspaceDialogTree (targetWorkspaceDialogTree: any): Promise<any> {
   try {
-    const lastNode: any = workspaceDialogTree?.result?.dialog_nodes?.find((node: any) => {
-      return node?.title?.toLowerCase().includes('lastnode')
+    const sourceNodesList: any = await createAssistantV1?.listDialogNodes({
+      workspaceId: WATSON_ASSISTANT_SOURCE_WORKSPACE_ID
+    }).then(nodesToExportResponse => {
+      return nodesToExportResponse.result.dialog_nodes
     })
 
-    if (!lastNode) {
+    const importNode: any = targetWorkspaceDialogTree?.result?.dialog_nodes?.find((node: any) => {
+      return node?.title?.toLowerCase().includes('import')
+    })
+
+    if (!importNode) {
       return {
         status: 400,
         error: 'Bad Request',
-        message: 'Last node not found'
+        message: 'Import node not found'
+      }
+    }
+
+    const entryPointNode: any = sourceNodesList?.find((node: any) => {
+      return node?.title?.toLowerCase().includes('entrypoint')
+    })
+
+    if (!entryPointNode) {
+      return {
+        status: 400,
+        error: 'Bad Request',
+        message: 'Entry point node required'
+      }
+    }
+
+    const nodesToExport = await sourceNodesList.filter((node: any) => {
+      return node?.title?.toLowerCase().includes('export')
+    })
+
+    if (!nodesToExport) {
+      return {
+        status: 400,
+        error: 'Bad Request',
+        message: 'Export nodes required'
       }
     }
 
     /// ------------Sets the entry point's previous_sibling with the node id of last node------ ///
-    entryPointNode.previous_sibling = lastNode?.dialog_node
+    entryPointNode.previous_sibling = importNode?.dialog_node
 
     return await createAssistantV1.updateWorkspace({
-      workspaceId: WATSON_ASSISTANT_WORKSPACE,
-      dialogNodes: workspaceDialogTree.result.dialog_nodes.concat(newDialogTree)
-    }).then(updateWorkspaceResponse => {
-      return updateWorkspaceResponse
+      workspaceId: WATSON_ASSISTANT_TARGET_WORKSPACE_ID,
+      dialogNodes: targetWorkspaceDialogTree.result.dialog_nodes.concat(entryPointNode, nodesToExport)
     })
+      .then(updateWorkspaceResponse => {
+        return updateWorkspaceResponse
+      })
   } catch (updateWorkspaceError) {
     return updateWorkspaceError
   }
